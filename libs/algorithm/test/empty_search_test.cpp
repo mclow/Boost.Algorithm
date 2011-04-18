c0e8fd9b44e9cb588df19d4ac39ee0c940b34e66
Marshall Clow <marshall@idio.com>
2011-04-18 08:25:42 -0700

    Added tests for empty patterns and corpuses (corpii?)

--- libs/algorithm/test/empty_search_test.cpp
+++ libs/algorithm/test/empty_search_test.cpp
@@ -4,18 +4,39 @@
 int main( int argc, char *argv [] )
 {
 	const std::string cs;
-	std::string ncs;
+	std::string estr;
+	std::string str ( "abc" );
 	
 //	Should compile successfully because the underlying types are the same.
 //	One is const and the other is not, but that's OK
 	(void) boost::algorithm::boyer_moore_search (
-		cs.begin (), cs.end (), ncs.begin (), ncs.end ());
+		cs.begin (), cs.end (), estr.begin (), estr.end ());
 
 	(void) boost::algorithm::boyer_moore_horspool_search (
-		cs.begin (), cs.end (), ncs.begin (), ncs.end ());
+		cs.begin (), cs.end (), estr.begin (), estr.end ());
 
 	(void) boost::algorithm::knuth_morris_pratt_search (
-		cs.begin (), cs.end (), ncs.begin (), ncs.end ());
+		cs.begin (), cs.end (), estr.begin (), estr.end ());
+
+//	empty corpus, non-empty pattern
+	(void) boost::algorithm::boyer_moore_search (
+		estr.begin (), estr.end (), str.begin (), str.end ());
+
+	(void) boost::algorithm::boyer_moore_horspool_search (
+		estr.begin (), estr.end (), str.begin (), str.end ());
+
+	(void) boost::algorithm::knuth_morris_pratt_search (
+		estr.begin (), estr.end (), str.begin (), str.end ());
+
+//	non-empty corpus, empty pattern
+	(void) boost::algorithm::boyer_moore_search (
+		str.begin (), str.end (), estr.begin (), estr.end ());
+
+	(void) boost::algorithm::boyer_moore_horspool_search (
+		str.begin (), str.end (), estr.begin (), estr.end ());
+
+	(void) boost::algorithm::knuth_morris_pratt_search (
+		str.begin (), str.end (), estr.begin (), estr.end ());
 
    (void) argv; (void) argc;
    return 0;
