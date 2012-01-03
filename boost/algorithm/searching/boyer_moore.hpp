3aa5d3a735d173fc844d8ac5575befb67f2f1c3e
Marshall Clow <marshall@idio.com>
2012-01-03 10:14:43 -0800

    Updated copyrights

--- boost/algorithm/searching/boyer_moore.hpp
+++ boost/algorithm/searching/boyer_moore.hpp
@@ -1,8 +1,8 @@
-/*
-    Copyright Marshall Clow 2010-2011. Use, modification and
-    distribution is subject to the Boost Software License, Version
-    1.0. (See accompanying file LICENSE_1_0.txt or copy at
-    http://www.boost.org/LICENSE_1_0.txt)
+/* 
+   Copyright (c) Marshall Clow 2010-2012.
+
+   Distributed under the Boost Software License, Version 1.0. (See accompanying
+   file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
 
     For more information, see http://www.boost.org
 */
--- libs/algorithm/test/hex_test3.cpp
+++ libs/algorithm/test/hex_test3.cpp
@@ -1,8 +1,10 @@
 /* 
-   Copyright (c) Marshall Clow 2008-2011.
+   Copyright (c) Marshall Clow 2011-2012.
 
    Distributed under the Boost Software License, Version 1.0. (See accompanying
    file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+
+    For more information, see http://www.boost.org
 
 Try ostream_iterators
 */
