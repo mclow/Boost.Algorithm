7434a8fe672e9a3a097a79010d8f7fc739e8027c
Alexander Zaitsev <zamazan4ik@tut.by>
2016-06-29 17:23:52 +0300

    Added tests and examples for is_palindromic()

--- boost/algorithm/is_palindromic.hpp
+++ boost/algorithm/is_palindromic.hpp
@@ -1,3 +1,17 @@
+/*
+  Copyright (c) Alexander Zaitsev <zamazan4ik@gmail.com>, 2016
+
+  Distributed under the Boost Software License, Version 1.0. (See
+  accompanying file LICENSE_1_0.txt or copy at
+  http://www.boost.org/LICENSE_1_0.txt)
+
+  See http://www.boost.org/ for latest version.
+*/
+
+/// \file  is_palindromic.hpp
+/// \brief Checks the input sequence on palindromic.
+/// \author Alexander Zaitsev
+
 #ifndef BOOST_ALGORITHM_is_palindromic_HPP
 #define BOOST_ALGORITHM_is_palindromic_HPP
 
@@ -8,8 +22,18 @@
 
 namespace boost {  namespace algorithm {
 
-template <typename BidirectionalIterator, typename Comp>
-bool is_palindromic(BidirectionalIterator begin, BidirectionalIterator end, Comp comp)
+/// \fn is_palindromic ( BidirectionalIterator begin, BidirectionalIterator end, Predicate p )
+/// \return true if the entire sequence is palindromic
+///
+/// \param begin    The start of the input sequence
+/// \param end		One past the end of the input sequence
+/// \param p        A predicate used to compare the values.
+///
+/// \note This function will return true for empty sequences and for palindromic
+///     sequences. For other sequences function will return false.
+///     Complexity: O(N).
+template <typename BidirectionalIterator, typename Predicate>
+bool is_palindromic(BidirectionalIterator begin, BidirectionalIterator end, Predicate p)
 {
     if(begin == end)
     {
@@ -19,7 +43,7 @@ bool is_palindromic(BidirectionalIterator begin, BidirectionalIterator end, Comp
     --end;
     while(begin != end)
     {
-        if(!comp(*begin, *end))
+        if(!p(*begin, *end))
         {
             return false;
         }
@@ -33,6 +57,15 @@ bool is_palindromic(BidirectionalIterator begin, BidirectionalIterator end, Comp
     return true;
 }
 
+/// \fn is_palindromic ( BidirectionalIterator begin, BidirectionalIterator end )
+/// \return true if the entire sequence is palindromic
+///
+/// \param begin    The start of the input sequence
+/// \param end		One past the end of the input sequence
+///
+/// \note This function will return true for empty sequences and for palindromic
+///     sequences. For other sequences function will return false.
+///     Complexity: O(N).
 template <typename BidirectionalIterator>
 bool is_palindromic(BidirectionalIterator begin, BidirectionalIterator end)
 {
@@ -40,18 +73,33 @@ bool is_palindromic(BidirectionalIterator begin, BidirectionalIterator end)
                          std::equal_to<typename std::iterator_traits<BidirectionalIterator>::value_type> ());
 }
 
-
+/// \fn is_palindromic ( const R& range )
+/// \return true if the entire sequence is palindromic
+///
+/// \param range The range to be tested.
+///
+/// \note This function will return true for empty sequences and for palindromic
+///     sequences. For other sequences function will return false.
+///     Complexity: O(N).
 template <typename R>
 bool is_palindromic(const R& range)
 {
     return is_palindromic(boost::begin(range), boost::end(range));
 }
 
-
-template <typename R, typename Comp>
-bool is_palindromic(const R& range, Comp c)
+/// \fn is_palindromic ( const R& range, Predicate p )
+/// \return true if the entire sequence is palindromic
+///
+/// \param range The range to be tested.
+/// \param p     A predicate used to compare the values.
+///
+/// \note This function will return true for empty sequences and for palindromic
+///     sequences. For other sequences function will return false.
+///     Complexity: O(N).
+template <typename R, typename Predicate>
+bool is_palindromic(const R& range, Predicate p)
 {
-    return is_palindromic(boost::begin(range), boost::end(range), c);
+    return is_palindromip(boost::begin(range), boost::end(range), p);
 }
 
 }}
